0045ba27048ff80acf8fd0966f0e961c44639991
Dustin <dustinwaggoner@comcast.net>
2016-09-15 10:08:55 -0700

    still working on aboutME template

---
+++ app/scripts/controllers/AboutMeCtrl.js
@@ -0,0 +1,9 @@
+(function() {
+  function AboutMeCtrl() {
+
+  }
+
+  angular
+    .module('myApp')
+    .controller('AboutMeCtrl', AboutMeCtrl);
+})();
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,5 +1,36 @@
 (function() {
+  function config($stateProvider, $locationProvider, $urlRouterProvider) {
+        $locationProvider
+            .html5Mode({ //hashBang URLs are disabled
+            enabled: true,
+            requireBase: false
+        });
+
+        $urlRouterProvider.otherwise("/home");
+
+        $stateProvider
+            .state('index', {
+                url: '/',
+                templateUrl: '/templates/home.html',
+                controller: 'HomeCtrl as home'
+            })
+            .state('my-projects', {
+              url:'my-projects',
+              templateUrl: '/templates/my-projects.html',
+              contoller: 'ProjectsCtrl'
+            })
+            .state('about-me', {
+              url: '/about-me',
+              templateUrl: '/templates/about-me.html',
+              controller: 'AboutMeCtrl'
+            })
+            .state('contact', {
+              url: 'contact-info',
+              templateUrl: 'templates/contact.html',
+            });
+      }
 
   angular
-    .module("myApp", []);
+    .module("myApp", ['ui.router'])
+    .config(config);
 })();

8720e5c44da32f2c3a48c518da3661f02febd421
Dustin <dustinwaggoner@comcast.net>
2016-10-13 09:33:18 -0700

    added to contact info page

---
+++ dist/scripts/services/ContactInfo.js
@@ -0,0 +1,12 @@
+(function() {
+  function ContactInfo() {
+    var ContactInfo = {};
+
+
+    return ContactInfo;
+  }
+
+  angular
+    .module('myApp')
+    .factory('ContactInfo', ContactInfo);
+})();
